1ca64176927ee6b73e6911da1da3fd7161005f49
Yo'av Moshe <yoav@cloudflare.com>
2022-06-06 20:25:18 +0200

    restore backlight settings after sleep

--- suspend.sh
+++ suspend.sh
@@ -2,10 +2,14 @@
 
 FILE=/tmp/sleeping
 if [ -f "$FILE" ]; then
+	brightnessctl -d "backlight_warm" s $(sed -n 0p $FILE)
+	brightnessctl -d "backlight_cold" s $(sed -n 1p $FILE)
 	rm $FILE
         notify-send "Welcome back" "Waking up..." -t 1000
 else
 	touch $FILE
+	brightnessctl -d "backlight_warm" g > $FILE
+	brightnessctl -d "backlight_cold" g > $FILE
 	brightnessctl -d "backlight_warm" s 0
 	brightnessctl -d "backlight_cold" s 0
 	sudo rfkill block 0
